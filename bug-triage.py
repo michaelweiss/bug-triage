759756035db79f19896b7eca843a0fe0abef66f5
michaelweiss <weiss@sce.carleton.ca>
2021-06-28 19:51:53 -0400

    add filter for number of contribs

--- bug-triage.py
+++ bug-triage.py
@@ -2,6 +2,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 import pandas as pd
 import numpy as np
+import math
 from pyvis.network import Network
 
 # model
@@ -12,17 +13,25 @@ def load_commits():
 	commits = commits[["source", "target", "weight", "start_date", "end_date", "year"]]
 	return commits
 
-def bipartite_graph(commits):
+def bipartite_graph(commits, min_contribs):
 	graph = Network("600px", "100%", notebook=True, heading='')
 	contributors = set(commits["source"])
 	bug_types = set(commits["target"])
 	id = 1
+	reverse_node_index = {}
 	for contributor in contributors:
 		graph.add_node(id, label=contributor, color="#00ee00")
+		reverse_node_index[contributor] = id
 		id = id + 1
 	for bug_type in bug_types:
 		graph.add_node(id, label=bug_type, color="#ee0000", shape="square")
+		reverse_node_index[bug_type] = id
 		id = id + 1
+	for i, row in commits.iterrows():
+		# st.write(i, row["source"], row["target"])
+		if row["weight"] > min_contribs:
+			graph.add_edge(reverse_node_index[row["source"]], reverse_node_index[row["target"]],
+				value=1 + math.log10(row["weight"]))
 	return graph
 
 	# for i in range(commits):
@@ -47,9 +56,10 @@ def show_commits():
 	st.dataframe(commits)
 
 def show_bipartite_graph():
-	st.title("Contributors-Bug type graph")
+	st.title("Contributors-bug type graph")
+	min_contribs = st.number_input("weight", 0)
 	commits = load_commits()
-	graph = bipartite_graph(commits)
+	graph = bipartite_graph(commits, min_contribs)
 	graph.show("contributor-bug-type.html")
 	with st.beta_container():
 		components.html(open("contributor-bug-type.html", 'r', encoding='utf-8').read(), height=625)
