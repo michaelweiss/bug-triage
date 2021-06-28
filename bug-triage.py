83b667a8d1a21bd5a928581140be3fdbee1179fa
michaelweiss <weiss@sce.carleton.ca>
2021-06-27 22:51:10 -0400

    initial commit

---
+++ bug-triage.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import streamlit.components.v1 as components
+import pandas as pd
+import numpy as np
+from pyvis.network import Network
+
+# model
+
+# view
+
+# controller
+
+st.sidebar.title("Bug triage")
+
