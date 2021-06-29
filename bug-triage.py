e1fdb7b1b96ffdc7f719c6f3aa731a0e54122e8d
michaelweiss <weiss@sce.carleton.ca>
2021-06-28 23:01:59 -0400

    add filter for total number of contribs

--- bug-triage.py
+++ bug-triage.py
@@ -13,62 +13,97 @@ def load_commits():
 	commits = commits[["source", "target", "weight", "start_date", "end_date", "year"]]
 	return commits
 
-def bipartite_graph(commits, min_contribs):
+def bipartite_graph(commits, min_contribs, max_contribs, min_total_contribs):
+	contribs = tally_contribs(commits)
 	graph = Network("600px", "100%", notebook=True, heading='')
-	contributors = set(commits["source"])
-	bug_types = set(commits["target"])
 	id = 1
 	reverse_node_index = {}
+
+	# compute the subset of contributors and bug types within range
+	contributors, bug_types = set(), set()
+	for i, row in commits.iterrows():
+		total_contribs = sum(contribs.loc[row["source"], :])
+		if total_contribs >= min_total_contribs:
+			if row["weight"] >= min_contribs: 
+				if min_contribs > max_contribs or row["weight"] <= max_contribs:
+					contributors.add(row["source"])
+					bug_types.add(row["target"])
+
+	# add nodes to the graph
 	for contributor in contributors:
-		graph.add_node(id, label=contributor, color="#00ee00")
+		total_contribs = sum(contribs.loc[contributor, :])
+		size = 10 * (1 + math.log10(total_contribs/max(1, min_contribs)))
+		graph.add_node(id, label=contributor, color="#00ee00", size=size, 
+			title="{}".format(total_contribs))
 		reverse_node_index[contributor] = id
 		id = id + 1
 	for bug_type in bug_types:
-		graph.add_node(id, label=bug_type, color="#ee0000", shape="square")
+		graph.add_node(id, label=bug_type, color="#ee0000", shape="square", size=10)
 		reverse_node_index[bug_type] = id
 		id = id + 1
+
+	# add edges to the graph
 	for i, row in commits.iterrows():
-		# st.write(i, row["source"], row["target"])
-		if row["weight"] > min_contribs:
-			graph.add_edge(reverse_node_index[row["source"]], reverse_node_index[row["target"]],
-				value=1 + math.log10(row["weight"]))
+		total_contribs = sum(contribs.loc[row["source"], :])
+		if total_contribs >= min_total_contribs:
+			if row["weight"] >= min_contribs: 
+				if min_contribs > max_contribs or row["weight"] <= max_contribs:
+					edge_width = 1 + math.log10(row["weight"])
+					graph.add_edge(reverse_node_index[row["source"]], reverse_node_index[row["target"]],
+						value=edge_width, title="{}".format(row["weight"]))
 	return graph
 
-	# for i in range(commits):
-	# 	graph.add_node(i, label=keywords[i], size=4*10*math.sqrt(total_topic_weights[i]),
-	# 		title="Topic {}".format(i))
-	# edge = np.zeros((number_of_topics, number_of_topics))
-	# for topic_weights in dtm:
-	# 	topics = [k for k in range(number_of_topics) if topic_weights[k] >= min_weight]
-	# 	for i, j in list(itertools.combinations(topics, 2)):
-	# 		edge[i, j] = edge[i, j] + 1
-	# for i in range(number_of_topics):
-	# 	for j in range(number_of_topics):
-	# 		if edge[i, j] >= min_edges:
-	# 			graph.add_edge(i, j, value=edge[i, j], smooth=smooth_edges)
-	# return graph
+def tally_contribs(commits):
+	contribs = {}
+	for i, row in commits.iterrows():
+		contributor = row["source"]
+		bug_type = row["target"]
+		number_of_contribs = row["weight"]
+		if contributor not in contribs:
+			contribs[contributor] = {}
+		contribs[contributor][bug_type] = number_of_contribs
+	for contributor in contribs:
+		row = [contribs[contributor][bug_type] if bug_type in contribs[contributor] else 0 
+			for bug_type in bug_types(commits)]
+		contribs[contributor] = row
+	return pd.DataFrame.from_dict(contribs, orient="index", columns=bug_types(commits))
+
+@st.cache()
+def bug_types(commits):
+	return sorted(list(set(commits["target"])))
 
 # view
 
 def show_commits():
 	st.title("Commits")
-	commits = load_commits()
-	st.dataframe(commits)
+	with st.beta_expander("More", expanded=False):
+		commits = load_commits()
+		st.dataframe(commits)
 
 def show_bipartite_graph():
 	st.title("Contributors-bug type graph")
-	min_contribs = st.number_input("weight", 0)
+	col1, col2, col3 = st.beta_columns(3)
+	min_contribs = col1.number_input("Min contributions (bug type", 0)
+	max_contribs = col2.number_input("Max contributions (bug type)", 0)
+	min_total_contribs = col3.number_input("Min total contributions", 0)
 	commits = load_commits()
-	graph = bipartite_graph(commits, min_contribs)
+	graph = bipartite_graph(commits, min_contribs, max_contribs, min_total_contribs)
 	graph.show("contributor-bug-type.html")
 	with st.beta_container():
 		components.html(open("contributor-bug-type.html", 'r', encoding='utf-8').read(), height=625)
+
+def show_tally_contribs():
+	st.title("Tally contributions")
+	with st.beta_expander("More", expanded=False):
+		commits = load_commits()
+		st.dataframe(tally_contribs(commits))
 
 # controller
 
 st.sidebar.title("Bug triage")
 
 show_commits()
+show_tally_contribs()
 show_bipartite_graph()
 
 
